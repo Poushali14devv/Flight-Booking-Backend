4e90357ae0c2c9bf319fc3bc1379092a0c272e56
Poushali14devv <poushalimajumder141102@gmail.com>
2026-04-18 20:00:48 +0530

    added tansaction to update seats

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -45,18 +45,37 @@ async getAllFlights(filter, sort) {
         return response;
     }
 
-    async updateRemainingSeats(flightId, seats, dec = true) {
-       await db.sequelize.query(addRowLockOnFlights(flightId));
-       const flight=await Flight.findByPk(flightId);
-       if(+dec){
-        await flight.decrement('totalseats',{by:seats});
+    // async updateRemainingSeats(flightId, seats, dec = true) {
+    //    const transaction =await db.sequelize.transaction();   
+    //    await db.sequelize.query(addRowLockOnFlights(flightId));
+    //    const flight=await Flight.findByPk(flightId);
+    //    if(+dec){
+    //     await flight.decrement('totalseats',{by:seats},{transaction:transaction});
         
-       }else{
-         await flight.increment('totalSeats',{by:seats});
+    //    }else{
+    //      await flight.increment('totalSeats',{by:seats},{transaction:transaction});
          
-       }
+    //    }
        
-       return flight;
+    //    return flight;
+       
+    // }
+     async updateRemainingSeats(flightId, seats, dec = true) {
+        const transaction = await db.sequelize.transaction();
+        try {
+            await db.sequelize.query(addRowLockOnFlights(flightId));
+            const flight = await Flight.findByPk(flightId);
+            if(+dec) {
+                await flight.decrement('totalSeats', {by: seats}, {transaction: transaction});
+            } else {
+                await flight.increment('totalSeats', {by: seats}, {transaction: transaction});
+            }
+            await transaction.commit();
+            return flight;
+        } catch(error) {
+            await transaction.rollback();
+            throw error;
+        }
        
     }
 }
