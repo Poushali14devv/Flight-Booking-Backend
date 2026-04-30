d0ace9846c1697932aca2432ab1eced9d0bba612
Poushali14devv <poushalimajumder141102@gmail.com>
2026-04-30 22:34:42 +0530

    made proxy complaint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-const express =require('express');
+// const express =require('express');
+
+// const { ServerConfig } = require('./config');
+// const apiRoutes = require('./routes');
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+
+// app.use('/api', apiRoutes);
+
+// app.listen(ServerConfig.PORT, () =>{
+//     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`)
+//     });
+
+
+
+ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -6,10 +24,16 @@ const apiRoutes = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
+// app.use('/flightsService/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () =>{
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`)
-    });
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+});
+
+/**
+ * In class task
+ * Setup flights table using sequelize
+ */
